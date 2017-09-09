328c78f83e747ea96be216433767bce8dcd63494
Ahmed Elhady <ahmed.elhadi@robustastudio.com>
2017-09-09 12:15:56 +0200

    create basice bucketlist model with tests

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,26 @@
+#!/usr/bin/env python
+"""
+Test case class for bucketlist model
+"""
 from django.test import TestCase
+from .models import Bucketlist
 
-# Create your tests here.
+class ModelTestCase(TestCase):
+    """
+    This class defines the test suite for the bucketlist model.
+    """
+    def setUp(self):
+        """
+        define the test client and other variables
+        """
+        self.bucketlist_name = "Write world class code"
+        self.bucketlist = Bucketlist(name=self.bucketlist_name)
+
+    def test_create_bucketlist(self):
+        """
+        Test the bucketlist model can create a bucketlist.
+        """
+        old_count = Bucketlist.objects.count()
+        self.bucketlist.save()
+        new_count = Bucketlist.objects.count()
+        self.assertEqual(old_count+1, new_count)
