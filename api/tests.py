68eaddde37f1db501c212bbceb767e5f1e69e3e4
Ahmed Elhady <ahmed.elhadi@robustastudio.com>
2017-09-10 16:49:28 +0200

    implemented details view for a bucketlist with tests

--- api/tests.py
+++ api/tests.py
@@ -44,3 +44,42 @@ class ViewTestCase(TestCase):
         Test the api has bucket creation capability.
         """
         self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
+
+    def test_api_get_bucketlist(self):
+        """
+        Test the api can get a given bucketlist.
+        """
+        bucketlist = Bucketlist.objects.get()
+        response = self.client.get(
+            reverse('details', kwargs={'pk': bucketlist.id}),
+            format="json"
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertContains(response, bucketlist)
+
+    def test_api_update_bucketlist(self):
+        """
+        Test the api can update a given bucketlist.
+        """
+        bucketlist = Bucketlist.objects.get()
+        new_bucketlist = {
+            "name": "New bucketlist"
+        }
+        response = self.client.put(
+            reverse('details', kwargs={'pk': bucketlist.id}),
+            new_bucketlist,
+            format="json"
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_api_delete_bucketlist(self):
+        """
+        Test the api can delete a given bucketlist.
+        """
+        bucketlist = Bucketlist.objects.get()
+        response = self.client.delete(
+            reverse('details', kwargs={'pk': bucketlist.id}),
+            format="json",
+            follow=True
+        )
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
